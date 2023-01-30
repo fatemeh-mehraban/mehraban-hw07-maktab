4f1813f8bdaa0439b0888a3f25c9f8017e0be754
mehraban <mehraban.fatemeh73@gmail.com>
2023-01-30 15:27:54 +0330

    edit tamrin3

---
+++ tamrin3/app-3.js
@@ -0,0 +1,6 @@
+let values = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+function clean(val){
+    // let x = new Set(val)
+ console.log([...new Set(val)]); 
+}
+clean(values)
