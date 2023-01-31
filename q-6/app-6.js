6d3ca0eb49fe8d4144969cb64aac73edab206c51
mehraban <mehraban.fatemeh73@gmail.com>
2023-01-31 23:58:52 +0330

    form validation

--- q-6/app-6.js
+++ q-6/app-6.js
@@ -1,18 +1,19 @@
 const inputs = document.querySelectorAll("input")
 let gender = document.getElementsByName('gender');
 const textinput= document.querySelectorAll('input[type="text"]')
-
-// console.log(textinput);
-// console.log(gender);
+let Name = document.getElementById("firstname")
+let lastname=document.getElementById("lastname")
+let email = document.getElementById("email")
+let form = document.querySelector("form")
 
 function myFunction(event) {
 event.preventDefault();
-
+    patternText()
     deletgenderandlog()
     displayRadioValue()
-    patternText()
-  }
 
+    
+  }
 
 
     function displayRadioValue() {
@@ -32,20 +33,84 @@ event.preventDefault();
 }
 }
     function patternText(){
-        
-        for(item of textinput){
-            if(item.value.length < 3){
-            place = item.parentNode
-            let span= document.createElement("span")
-            span.innerHTML = ` ${item.id} entered is incorrect!`
-            span.style.fontSize = "17px"
-            span.style.padding = "5px"
-            span.style.color = "red"
-            place.appendChild(span)
-            item.style.border = "2px solid red"
-           
+        if( Name.value.length <3){
+            place = Name.parentNode
+             let span= document.createElement("span")
+             span.className = "eror"
+             span.innerHTML = ` ${Name.id} entered is incorrect!`
+             span.style.fontSize = "17px"
+             span.style.padding = "5px"
+             span.style.color = "red"
+             place.appendChild(span)
+             Name.style.border = "2px solid red"
+             form.dataset.invalid == 1
         }
-       
-    }
-    }
-   
+        Name.addEventListener("change", ()=>{
+            Name.parentElement.lastChild.remove();
+            Name.style.border = "none"
+        })
+
+        if( lastname.value.length <3){
+            place = lastname.parentNode
+             let span= document.createElement("span")
+             span.className = "eror"
+             span.innerHTML = ` ${lastname.id} entered is incorrect!`
+             span.style.fontSize = "17px"
+             span.style.padding = "5px"
+             span.style.color = "red"
+             place.appendChild(span)
+             lastname.style.border = "2px solid red"
+             form.dataset.invalid == 1
+        }
+        lastname.addEventListener("change", ()=>{
+            lastname.parentElement.lastChild.remove();
+            lastname.style.border = "none"
+        })
+
+
+        // if( !email.value.contains("@")){
+        //     place = email.parentNode
+        //      let span= document.createElement("span")
+        //      span.className = "eror"
+        //      span.innerHTML = ` ${email.id} entered is incorrect!`
+        //      span.style.fontSize = "17px"
+        //      span.style.padding = "5px"
+        //      span.style.color = "red"
+        //      place.appendChild(span)
+        //      email.style.border = "2px solid red"
+        //      form.dataset.invalid == 1
+        // }
+        email.addEventListener("change", ()=>{
+            email.parentElement.lastChild.remove();
+            email.style.border = "none"
+        })
+
+
+
+
+
+
+
+
+        
+    //     for(item of textinput){
+    //         if(item.value.length < 3 ){
+    
+    //                     place = item.parentNode
+    //                     let span= document.createElement("span")
+    //                     span.className = "eror"
+    //                     span.innerHTML = ` ${item.id} entered is incorrect!`
+    //                     span.style.fontSize = "17px"
+    //                     span.style.padding = "5px"
+    //                     span.style.color = "red"
+    //                     place.appendChild(span)
+    //                     item.style.border = "2px solid red"
+    //         }
+    // }
+}
+// function formvalid() {
+//     if(form.dataset.invalid == 1){
+//         return null
+//     }
+// }
+
